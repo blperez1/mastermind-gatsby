233bb7d091ca72637c205ef51370f6df8c62a14b
Unknown <blossomperez18@gmail.com>
2020-11-13 21:44:05 -0600

    Create Header component that contains instructions and game buttons

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Button from "../components/button"
+
+const Header = () => {
+    return (
+        <>
+            <h1 id="top">Break Me</h1>
+            <p>(check console for description)</p>
+            <h2 className="colorDescription"><span className="red">Red</span> = right color right spot | <span className="white">White</span> = right color wrong spot</h2>
+            <h5 className="thin">(no particular order)</h5><br/>
+            <Button key={"increaseDiff"} context="Hard Mode"/>
+            <p id="isHardMode"></p>
+		    <p className="otherColors">(Hard Mode Adds: <span className="grey">Grey</span>, <span className="cyan">Cyan</span>, <span
+				className="olive">Olive</span>, <span className="tan">Tan</span>, <span className="purple">Purple</span> and <span
+				className="lime">Lime</span>)</p>
+		    <Button key="newGame" context="Start Game"/>
+            <p className="easyOptions"><span><strong>Options: </strong></span><span className="blue">Blue</span>, <span
+				className="brown">Brown</span>, <span className="yellow">Yellow</span>, <span className="orange">Orange</span>,
+			<span className="green">Green</span> and <span className="pink">Pink</span></p>
+        </>
+    )
+}
+
+export default Header
