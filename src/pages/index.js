4b75fe27bae844c89a8d8aa2f3252d72e3e275a4
Unknown <blossomperez18@gmail.com>
2020-11-13 21:44:22 -0600

    Add index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
+import Header from "../components/header"
+import Gameboard from "../components/gameboard"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <Layout> 
+            <Header/>
+            <Gameboard/>
+          </Layout>
 }
