329c5eaf708457e1e9f84486375ab20dc99b7249
Unknown <blossomperez18@gmail.com>
2020-11-14 09:32:33 -0600

    Add comments
    -Added comments to keep track of element ids that are not rendering

--- src/components/header.js
+++ src/components/header.js
@@ -8,12 +8,14 @@ const Header = () => {
             <p>(check console for description)</p>
             <h2 className="colorDescription"><span className="red">Red</span> = right color right spot | <span className="white">White</span> = right color wrong spot</h2>
             <h5 className="thin">(no particular order)</h5><br/>
+            {/* id not rendered */}
             <Button key={"increaseDiff"} context="Hard Mode"/>
             <p id="isHardMode"></p>
 		    <p className="otherColors">(Hard Mode Adds: <span className="grey">Grey</span>, <span className="cyan">Cyan</span>, <span
 				className="olive">Olive</span>, <span className="tan">Tan</span>, <span className="purple">Purple</span> and <span
 				className="lime">Lime</span>)</p>
-		    <Button key="newGame" context="Start Game"/>
+                {/* id not rendered */}
+		    <Button key={"newGame"} context="Start Game"/>
             <p className="easyOptions"><span><strong>Options: </strong></span><span className="blue">Blue</span>, <span
 				className="brown">Brown</span>, <span className="yellow">Yellow</span>, <span className="orange">Orange</span>,
 			<span className="green">Green</span> and <span className="pink">Pink</span></p>
