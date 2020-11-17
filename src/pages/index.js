754a858a352c1f4c84007d088e52a9edeb7f1bab
Unknown <blossomperez18@gmail.com>
2020-11-17 16:48:33 -0600

    Add AnswerKey component tag to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import Gameboard from "../components/gameboard"
+import AnswerKey from "../components/answerkey"
 
 export default function Home() {
-  return <Layout> 
+  return <Layout>  
             <Header/>
             <Gameboard/>
+            <AnswerKey/>
           </Layout>
 }
